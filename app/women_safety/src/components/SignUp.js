b175db482c937c95670bb0e5d7c7187e72241ee7
Shail769 <shail.shah822@gmail.com>
2020-03-07 19:23:37 +0530

    Added SignUp

---
+++ app/women_safety/src/components/SignUp.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import { Text, TextInput, View, TouchableHighlight } from 'react-native'
+
+
+function row (number = ""){
+
+
+    return(
+        <View style={}>
+            
+        </View>
+    )
+}
+
+
+
+export default class SignUp extends Component {
+
+    constructor(){
+        super(this.props);
+        this.state={
+            name : '',
+            otp : '',
+            phu : '',
+            ph1 : '',
+            ph2 : '',
+            ph3 : '',
+            ph4 : '',
+            ph5 : '', 
+        }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value,
+        })
+    };
+
+
+
+
+    render() {
+        return (
+            <View>
+                <View style={styles.helpmsg} >
+                    <Text> Name: </Text>
+                    <TextInput
+                        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                        onChangeText={(helpText) => this.setState({helpText})}
+                        name="name"
+                        value={this.state.name}
+                    />
+                </View>
+                <View style={styles.helpmsg} >
+                    <Text> Name: </Text>
+                    <TextInput
+                        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                        onChangeText={(helpText) => this.setState({helpText})}
+                        name="name"
+                        value={this.state.name}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    helpmsg:{
+        flex:1,
+        flexDirection:'row'
+    },
+    contactHeader:{
+        flex:1,
+        flexDirection:'row'    
+    },
+    contactAddBtn:{
+    }
+});
+
