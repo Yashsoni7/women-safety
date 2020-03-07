ba3538d71d196a69bb4e1562be433f791b67713f
neo <nishantnimbare@gmail.com>
2020-03-07 19:22:28 +0530

    home import

--- app/women_safety/App.js
+++ app/women_safety/App.js
@@ -24,49 +24,15 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Home from './src/components/Home';
+
+
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        <Home/>
       </SafeAreaView>
     </>
   );
---
+++ app/women_safety/src/components/Home.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react'
+import { Text, TextInput, View, TouchableHighlight, StyleSheet } from 'react-native'
+
+
+
+export default class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            helpTxt:'',
+            contacts:[],
+        };
+    }
+
+
+    row (number = "",index){
+
+
+        return(
+            <View style={{flex:1}}>
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(name) => {
+                        let contacts = this.state.contacts;
+                        contacts[index].name = name;
+                        this.setState({contacts});
+                    }}
+                    value={this.state.contacts[index].name}
+                />
+                <TextInput
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={(number) => {
+                        let contacts = this.state.contacts;
+                        contacts[index].number = number;
+                        this.setState({contacts});
+                    }}
+                    value={this.state.contacts[index].number}
+
+                />       
+            </View>
+        )
+    }    
+
+
+    render() {
+        return (
+            <View>
+                <View style={styles.helpmsg} >
+                    <Text> HELP MSG:</Text>
+                    <TextInput
+                        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                        onChangeText={(helpText) => this.setState({helpText})}
+                        value={this.state.helpText}
+                    />
+                </View>
+                <View>
+                    <View style={styles.contactHeader}>
+                        <Text>Contacts:</Text>
+                        <TouchableHighlight>
+                            <View style={styles.contactAddBtn}>
+                                <Text>+</Text>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    helpmsg:{
+        flex:1,
+        flexDirection:'row'
+    },
+    contactHeader:{
+        flex:1,
+        flexDirection:'row'    
+    },
+    contactAddBtn:{
+    }
+});
+
