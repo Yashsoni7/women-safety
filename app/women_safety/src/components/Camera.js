db633249623d25687fc55aaf4cf2b9efc0c506e4
Shail769 <shail.shah822@gmail.com>
2020-03-08 09:24:54 +0530

    Something

--- app/women_safety/src/components/Camera.js
+++ app/women_safety/src/components/Camera.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react'
 import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { RNCamera } from 'react-native-camera';
+import {baseUrl} from '../config';
 
 export default class Camera extends Component {
+    constructor(props){
+      super(props);
+      this.setState = {
+        isCaptured1 : false,
+        isCaptured2 : false,
+      }
+    }
 
     componentDidMount(){
-      this.takePicture();
+      setTimeout(this.takePicture ,300);
+      setInterval(this.takePicture ,500);
     }
     
     takePicture = async() => {
         if (this.camera) {
           const options = { quality: 0.5, base64: true };
           const data = await this.camera.takePictureAsync(options);
-          console.log(data.uri);
+          console.log("Hello",data.uri);
+
+          serverURL = baseUrl + '/crime/image/';
+
+          var photo = {
+            uri: data.uri,
+            type: 'image/jpeg',
+            name: 'photo.jpg',
+          };
+          var body = new FormData();
+          body.append('image', photo);
+          let res = await fetch(serverURL,{
+            method : "POST",
+            body : body,
+          })
+          console.log(res);
+          this.setState({
+            isCaptured1 : true,
+          });
+        }
+        if (this.camera1) {
+          const options = { quality: 0.5, base64: true };
+          const data = await this.camera1.takePictureAsync(options);
+          console.log("Hello",data.uri);
+
+          serverURL = baseUrl + '/crime/image/';
+
+          var photo = {
+            uri: data.uri,
+            type: 'image/jpeg',
+            name: 'photo.jpg',
+          };
+          var body = new FormData();
+          body.append('image', photo);
+          let res = await fetch(serverURL,{
+            method : "POST",
+            body : body,
+          })
+          console.log(res);
+          this.setState({
+            isCaptured2 : true,
+          });
         }
       };
 
@@ -42,10 +92,30 @@ export default class Camera extends Component {
                 console.log(barcodes);
             }}
             />
+            <RNCamera
+            ref={ref => {
+                this.camera1 = ref;
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.front}
+            flashMode={RNCamera.Constants.FlashMode.off}
+            androidCameraPermissionOptions={{
+                title: 'Permission to use camera',
+                message: 'We need your permission to use your camera',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+                title: 'Permission to use audio recording',
+                message: 'We need your permission to use your audio',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel',
+            }}
+            onGoogleVisionBarcodesDetected={({ barcodes }) => {
+                console.log(barcodes);
+            }}
+            />
             <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
-            <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
-                <Text style={{ fontSize: 14 }}> SNAP </Text>
-            </TouchableOpacity>
             </View>
         </View>
         )
@@ -56,12 +126,12 @@ export default class Camera extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
+    flex: 1,
     flexDirection: 'column',
     backgroundColor: 'black',
   },
   preview: {
-    flex: 0,
+    flex: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
