585a0bc4711fc5955bcf8c7319bd5366d931dc95
Yash Soni <yashsoni77779@gmail.com>
2020-03-08 16:41:06 +0530

    Final Commit

--- backend/women_safety/crime/views.py
+++ backend/women_safety/crime/views.py
@@ -7,12 +7,18 @@ from user.models import women
 import requests
 import json
 from rest_framework.parsers import FileUploadParser
+from datetime import datetime, timedelta
+import pytz
+from .models import CrimeImages
 
 @api_view(['POST'])
 def ReportCrime(request):
     if request.method == 'POST':
         data = request.data
-        ser = CrimeSerializer(data=request.data)    
+        req = requests.get('https://us1.locationiq.com/v1/reverse.php?key=c3878484ac573f&lat=' + str(data['lattitude']) + '&lon=' + str(data['longitude']) + '&format=json')
+        dat = json.loads(req.text)
+        data.update({"address":dat["display_name"],"time":datetime.now() + timedelta(hours=5.5)})
+        ser = CrimeSerializer(data=request.data)
 
         if ser.is_valid():
             ser.save()
@@ -28,10 +34,7 @@ def ReportCrime(request):
                 res.update({"emergency_contact4":cont.emergency_contact4})
             if (cont.emergency_contact5):
                 res.update({"emergency_contact5":cont.emergency_contact5})
-            req = requests.get('https://us1.locationiq.com/v1/reverse.php?key=c3878484ac573f&lat=' + str(data['lattitude']) + '&lon=' + str(data['longitude']) + '&format=json')
-            dat = json.loads(req.text)
-            res.update({"address":dat["display_name"]})
-            print(res)
+            res.update({"address":data["address"]})
             return Response(res)
         return Response(ser.errors)
 
@@ -39,8 +42,12 @@ def ReportCrime(request):
 def ShowCrimes(request):
     if request.method == 'GET':
         crime_obj = Crime.objects.filter(helped=False)
-        ser = CrimeSerializer(crime_obj, many=True)
-        return Response(ser.data)
+        print(crime_obj)
+        li = []
+        for i in crime_obj:
+            li.append({"name":women.objects.get(pk=i.number.phone_number).name,"number":i.number.phone_number,"address":i.address,"lattitude":i.lattitude,"longitude":i.longitude, "time":i.time.strftime("%m/%d/%Y, %H:%M:%S")})
+            print(type(i.time))
+        return Response(li)
 
 @api_view(['POST'])
 def SendImages(request):
@@ -53,3 +60,10 @@ def SendImages(request):
             print(ser.data)
             return Response(ser.data)
         return Response(ser.errors)
+
+@api_view(['GET'])
+def ShowImages(request):
+    if request.method == "GET":
+        img = CrimeImages.objects.all()
+        ser = CrimeImageSerializer(img,many=True)
+        return Response(ser.data)
